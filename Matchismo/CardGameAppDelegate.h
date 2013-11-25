4ebd9f41140456c593022349afd817adaf12281f
Kelv <kelv@Kelvs-MacBook-Pro.local>
2013-11-25 21:31:10 +0000

    Initial Commit

---
+++ Matchismo/CardGameAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CardGameAppDelegate.h
+//  Matchismo
+//
+//  Created by Kelv on 25/11/2013.
+//  Copyright (c) 2013 Kelv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Matchismo/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  Matchismo
+//
+//  Created by Kelv on 25/11/2013.
+//  Copyright (c) 2013 Kelv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
