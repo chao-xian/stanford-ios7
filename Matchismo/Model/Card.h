bb4ccb020eb905b32274e5dc3f3d58607308eef9
Kelv <kelvingan+github@gmail.com>
2013-11-28 17:12:55 +0000

    Created stub Card classes

---
+++ Matchismo/Model/Card.h
@@ -0,0 +1,13 @@
+//
+//  Card.h
+//  Matchismo
+//
+//  Created by Kelv on 28/11/2013.
+//  Copyright (c) 2013 Kelv. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Card : NSObject
+
+@end
---
+++ Matchismo/Model/Card.m
@@ -0,0 +1,13 @@
+//
+//  Card.m
+//  Matchismo
+//
+//  Created by Kelv on 28/11/2013.
+//  Copyright (c) 2013 Kelv. All rights reserved.
+//
+
+#import "Card.h"
+
+@implementation Card
+
+@end
