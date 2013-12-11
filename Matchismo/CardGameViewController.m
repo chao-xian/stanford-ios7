5f7d3bceae60ab8857c7b05d945290313514c596
Kelv <kelvingan+github@gmail.com>
2013-12-11 22:08:13 +0000

    AT LAST finished first assignment...

--- Matchismo/CardGameViewController.m
+++ Matchismo/CardGameViewController.m
@@ -7,10 +7,14 @@
 //
 
 #import "CardGameViewController.h"
+#import "Deck.h"
+#import "PlayingCard.h"
+#import "PlayingCardDeck.h"
 
 @interface CardGameViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
 @property (nonatomic) int flipCount;
+@property (strong, nonatomic) Deck *deck;
 @end
 
 @implementation CardGameViewController
@@ -22,18 +26,31 @@
 }
 
 - (IBAction)touchCardButton:(UIButton *)sender {
-
-    if ([sender.currentTitle length]) {
-        [sender setBackgroundImage:[UIImage imageNamed:@"cardback"]
-                          forState:UIControlStateNormal];
-        [sender setTitle:@"" forState:UIControlStateNormal];
-    } else {
-        [sender setBackgroundImage:[UIImage imageNamed:@"cardfront"]
-                          forState:UIControlStateNormal];
-        [sender setTitle:@"A♣️" forState:UIControlStateNormal];
-    }
-    self.flipCount++;
     
+    PlayingCard *card = nil;
+    
+    if (self.flipCount < 52) {
+
+        if ([sender.currentTitle length]) {
+            [sender setBackgroundImage:[UIImage imageNamed:@"cardback"]
+                              forState:UIControlStateNormal];
+            [sender setTitle:@"" forState:UIControlStateNormal];
+        } else {
+            [sender setBackgroundImage:[UIImage imageNamed:@"cardfront"]
+                              forState:UIControlStateNormal];
+            card = (PlayingCard *)[self.deck drawRandomCard];
+            
+            [sender setTitle:card.contents forState:UIControlStateNormal];
+        }
+        self.flipCount++;
+    }
+    
+}
+
+- (Deck *)deck
+{
+    if (!_deck) _deck = [[PlayingCardDeck alloc] init];
+    return _deck;
 }
 
 
